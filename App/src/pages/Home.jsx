0adccb9fb83fa725cd65a3639e17a14559d9c002
Loyd <us.flores.loyd@gmail.com>
2024-02-04 04:01:27 -0500

    Addition of learn on landing page and addition of comments and dialogues

--- App/src/pages/Home.jsx
+++ App/src/pages/Home.jsx
@@ -23,12 +23,12 @@ function Home() {
               <p>Discover the universe and beyond...</p>
             </div>
             <div>
-              <h1>Mission to Mars</h1>
-              <p>Experience the Red Planet like never before.</p>
+              <h1>3D Rocket Model</h1>
+              <p>Unleash the Cosmos: Adventure Awaits Beyond the Horizon.</p>
             </div>
             <div>
-              <h1>Orbital Outposts</h1>
-              <p>Building the future in space.</p>
+            <h1>Physics Game Engine</h1>
+              <p>Soar Through the Heavens, Where Only Rockets Dare</p>
             </div>
             {/* Add more slides as needed */}
           </Slider>
@@ -39,15 +39,34 @@ function Home() {
           <Slider {...settings}>
             <div>
               <h1>Rocket Builder</h1>
-              <p>Discover the universe and beyond...</p>
+              <p>Where Engineering Meets Imagination: Assemble the Future.</p>
             </div>
             <div>
-              <h1>Mission to Mars</h1>
-              <p>Experience the Red Planet like never before.</p>
+            <h1>Component Library</h1>
+              <p>Explore the Building Blocks of Space Travel.</p>
             </div>
             <div>
-              <h1>Orbital Outposts</h1>
-              <p>Building the future in space.</p>
+            <h1>C02 Launch Calculator</h1>
+              <p>From Blueprint to Liftoff: Every Part Tells a Story.</p>
+            </div>
+            {/* Add more slides as needed */}
+          </Slider>
+        </div>
+      </div>
+      <div className="third-section">
+        <div className="floating-text">
+          <Slider {...settings}>
+            <div>
+              <h1>Learn</h1>
+              <p>Diving into the vastness of space, we realize how boundless our curiosity can be. Every star and planet tells a unique story of cosmic wonder.</p>
+            </div>
+            <div>
+              <h1>Advocate and Educate</h1>
+              <p>Tackling CO2 emissions starts with knowledge.</p>
+            </div>
+            <div>
+            <h1>Takei</h1>
+              <p>The best professor!</p>
             </div>
             {/* Add more slides as needed */}
           </Slider>
